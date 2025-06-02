106822c0b9dd8257873248868984edafadca6e5d
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2025-06-02 03:05:40 +0000

    03_Practica2

--- Pracitica2.py
+++ Pracitica2.py
@@ -20,3 +20,12 @@ def mostrar_productos(lista):
         for j in range(n - i - 1):
             if productos[j].precio > productos[j + 1].precio:
                 productos[j], productos[j + 1] = productos[j + 1], productos[j]
+
+                def ordenar_seleccion(productos):
+    n = len(productos)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if productos[j].precio < productos[min_idx].precio:
+                min_idx = j
+        productos[i], productos[min_idx] = productos[min_idx], productos[i]
