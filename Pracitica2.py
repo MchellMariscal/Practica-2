e82ae2d4b24d07160d0cc61a437387d86428afa2
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2025-06-02 03:05:12 +0000

    02_Practica2

--- Pracitica2.py
+++ Pracitica2.py
@@ -13,3 +13,10 @@ def mostrar_productos(lista):
     for producto in lista:
         print(producto)
     print()
+
+    def ordenar_burbuja(productos):
+    n = len(productos)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if productos[j].precio > productos[j + 1].precio:
+                productos[j], productos[j + 1] = productos[j + 1], productos[j]
